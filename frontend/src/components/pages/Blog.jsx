a3186a1ff03f7ab73f86bed86c283d39ddc7ddc5
Nguyen Thi Thu Thao <nttt603204@gmail.com>
2026-04-16 21:08:13 +0700

    1

--- frontend/src/components/pages/Blog.jsx
+++ frontend/src/components/pages/Blog.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
-import axios from './api/axios';
+import axios from '../../api/axios';
 
 const Blog = () => {
   const [posts, setPosts] = useState([]);
